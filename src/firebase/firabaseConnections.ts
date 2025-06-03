fd97e75d9d1e4fc49788a3f9aca1f080bbc28b8a
rafaeltws <159142263+rafaeltws@users.noreply.github.com>
2025-06-03 15:47:06 -0300

    Update firabaseConnections.ts

--- src/firebase/firabaseConnections.ts
+++ src/firebase/firabaseConnections.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjIuJTr1KKh1bd5mSuV0jTA1AuV6VUhc8",
-  authDomain: "reactlinktree-c1d08.firebaseapp.com",
-  projectId: "reactlinktree-c1d08",
-  storageBucket: "reactlinktree-c1d08.firebasestorage.app",
-  messagingSenderId: "1056549153067",
-  appId: "1:1056549153067:web:10e188f38a99d90ed83d6e"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
